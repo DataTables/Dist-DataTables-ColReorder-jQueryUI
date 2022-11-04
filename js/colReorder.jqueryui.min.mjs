398e9467a182061b8aa5e5d7023a5d232048cf89
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:12:21 +0000

    6311707142236e2c1c5cb6fea9602a763da11d07
    Sync to source repo @6311707142236e2c1c5cb6fea9602a763da11d07

--- js/colReorder.jqueryui.min.mjs
+++ js/colReorder.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI styling wrapper for ColReorder
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-colreorder";export default DataTable;
