cb33b062092da9b539dc6b13fca2f7ac88b7b66a
dtbuild <dtbuild@rigil>
2026-07-22 13:35:37 +0000

    a5f4bf12f9e29eaa65543175ca82252940d83634 Release 3.0.0
    Sync to source repo @a5f4bf12f9e29eaa65543175ca82252940d83634

--- js/colReorder.jqueryui.min.mjs
+++ js/colReorder.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! ColReorder jQuery UI styling 3.0.0-beta.1 for DataTables
+/*! ColReorder jQuery UI styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import ColReorder from"datatables.net-colreorder";export default DataTable;
--- js/colReorder.jqueryui.mjs
+++ js/colReorder.jqueryui.mjs
@@ -1,4 +1,4 @@
-/*! ColReorder jQuery UI styling 3.0.0-beta.1 for DataTables
+/*! ColReorder jQuery UI styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
